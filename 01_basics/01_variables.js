ab06bb584eb41c616c6bd926cbfa37a7390fef56
Tanish Ramchandani <ramchandanitanishq@gmail.com>
2024-02-23 16:08:10 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1001
+let accountEmail = "abc@google.com"
+var accountPassword = "12345"
+accountCity = "Ahmedabad"
+let accountState;
+
+accountEmail = "def@ggl.com"
+accountPassword = "67890"
+accountCity = "Goa"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
