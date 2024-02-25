105d47496b735d70e62bde85470bb6ddf47b1957
Tanish Ramchandani <ramchandanitanishq@gmail.com>
2024-02-25 16:18:30 +0000

    prefix and postfix

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,16 +1,35 @@
-let score = "26abc"
+// let score = "26abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-//NaN => not a number
-//true => 1
-//false => 0
-let isloggedin = null
-let booleanisloggedin = Boolean(isloggedin)
-console.log(typeof booleanisloggedin);
-//1 => true; 0 => false
-// "" => false; "<value>" => true
+// //NaN => not a number
+// //true => 1
+// //false => 0
+// let isloggedin = null
+// let booleanisloggedin = Boolean(isloggedin)
+// console.log(typeof booleanisloggedin);
+// //1 => true; 0 => false
+// // "" => false; "<value>" => true
+
+// ********************** Operations ******************
+
+// let value = 26
+// let negvalue = -value
+// console.log(negvalue);
+// let str1 = "hello"
+// let str2 = " Tanishq"
+// let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+console.log(+true);
+console.log(+"");
+let gameCounter = 101
+gameCounter++;
+console.log(gameCounter);
