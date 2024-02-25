ab6de7f911411aa7dacd34a646504fa42f6f5011
Tanish Ramchandani <ramchandanitanishq@gmail.com>
2024-02-25 15:07:11 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,14 @@
 "use strict"; // treat all JS code as newer version
 
 //alert("welcome dosto") //we're using node JS, not browser
+//number => 2^53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof null);
