2094d454034ef75ef8be3204d4f3a5b1fd93555f
Tanish Ramchandani <ramchandanitanishq@gmail.com>
2024-02-25 15:40:53 +0000

    conversions

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "26abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//NaN => not a number
+//true => 1
+//false => 0
+let isloggedin = null
+let booleanisloggedin = Boolean(isloggedin)
+console.log(typeof booleanisloggedin);
+//1 => true; 0 => false
+// "" => false; "<value>" => true
