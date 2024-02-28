6fdb3bb458553c7eca280e37bc6a3067d161da85
Tanish Ramchandani <ramchandanitanishq@gmail.com>
2024-02-28 17:22:22 +0000

    More about Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,23 @@
+const name = "Tanishq"
+const repoCount = 2
+// console.log(name + repoCount + "value");
+// console.log(`Hello my name is ${name} and I have ${repoCount} repositories`); // better syntax
+const game = new String('Far Cry') // another way to declare string
+// console.log(game[2]);
+// console.log(game.__proto__);
+// console.log(game.length);
+// console.log(game.toUpperCase());
+// console.log(game.charAt(4));
+// console.log(game.indexOf('C'));
+// const newGame = game.substring(1, 6)
+// console.log(newGame);
+// const game2 = game.slice(1,6)
+// console.log(game2);
+const stringOne = "      Tanishq     "
+console.log(stringOne);
+console.log(stringOne.trim()); 
+const url = "https://tanishhr.com/Contact%20Us"
+
+console.log(url.replace('%20', '-'));
+console.log(url.includes('tanishhr'));
+console.log(game.split(' '));
