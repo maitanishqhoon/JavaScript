4a419acba7b2da68c07a3ebe550e0a73947cbaeb
Tanish Ramchandani <ramchandanitanishq@gmail.com>
2024-02-28 18:06:47 +0000

    Numbers and Math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,33 @@
+const score = 7000
+// console.log(score);
+
+const balance = new Number(10250)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const float = 119.11
+
+//console.log(float.toPrecision(3));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-8));
+// console.log(Math.round(3.9));
+// console.log(Math.ceil(3.1));
+// console.log(Math.floor(3.9));
+// console.log(Math.min(69, 6, 9, 6.9, 0.6, 0.9));
+// console.log(Math.max(69, 6, 9, 6.9, 0.6, 0.9));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*100) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
