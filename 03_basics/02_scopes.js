d2aa8e15d0c977fe511010ff260c3c13a40e7c11
Tanish Ramchandani <ramchandanitanishq@gmail.com>
2024-03-10 12:16:53 +0000

    more about scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,8 +4,46 @@ if(true){
     let a = 10
     const b = 20
     var c = 30
-    console.log("INNER: ",a);
+    //console.log("INNER: ",a);
 }
-console.log(a);
+//console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+    const username = "Tnix"
+    
+    function two() {
+        const website = "youtube"
+        //console.log(username);
+    }
+    //console.log(website);
+
+    //two()
+
+}
+
+//one()
+
+if(true){
+    const username = "fake"
+    if (username === "fake") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ************************** interesting ****************************
+console.log(addone(68));
+function addone(num) {
+    return num+1
+}
+
+
+addtwo(67)
+const addtwo = function(num) {
+    return num+2
+}
+
